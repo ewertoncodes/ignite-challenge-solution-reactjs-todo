751806352529aa74ac0eafa44407edb0b0ceb230
ewertoncodes <ewertonorg@gmail.com>
2025-02-09 23:45:48 -0300

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+import todoLogo from '../assets/todo-logo.svg'
+
+console.log()
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} alt="" />
+    </header>
+  )
+}
