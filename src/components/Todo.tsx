09f0d86e222c40d21b2f74346618b86a7e58892c
ewertoncodes <ewertonorg@gmail.com>
2025-02-10 23:54:04 -0300

    add plus icon

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,10 +1,14 @@
 import styles from '../components/Todo.module.css'
+import { PlusCircle } from "@phosphor-icons/react";
 
 export function Todo() {
   return (
     <div className={styles.inputContainer}>
       <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button className={styles.addButton}>Criar</button>
+      <button className={styles.addButton}>
+        Criar
+        <PlusCircle size={20} />
+      </button>
     </div>
   )
 }
