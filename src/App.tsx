e93e576a0a11229e90658305deee89095003508c
ewertoncodes <ewertonorg@gmail.com>
2025-02-13 01:18:54 -0300

    add todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Header } from "./components/Header"
 import { Todo } from "./components/Todo"
+import { TodoList } from "./components/TodoList"
+
+import  styles  from './App.module.css'
 
 export function App() {
   return (
     <>
       <Header />
-      <Todo />
+      <div className={styles.container}>
+        <Todo />
+        <TodoList />
+      </div>
     </>
   )
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,26 @@
+import styles from './TodoList.module.css';
+import { ClipboardText } from "@phosphor-icons/react";
+
+export function TodoList() {
+  return (
+    <div className={styles.todoList}>
+      <header>
+        <aside>
+          Tarefas criadas <span>0</span>
+        </aside>
+        <aside>
+          Concluidas <span>0</span>
+        </aside>
+      </header>
+      <div className={styles.emptyState}>
+        <div className={styles.iconContainer}>
+          <ClipboardText size={50} />
+        </div>
+        <div className={styles.textContainer}>
+          <p>Você ainda não tem tarefas cadastradas.</p>
+          <p>Crie tarefas e organize seus itens a fazer.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
