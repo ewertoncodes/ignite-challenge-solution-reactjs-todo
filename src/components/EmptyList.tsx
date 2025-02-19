436633d91217a562c817bac25d0d5b4affb4d362
ewertoncodes <ewertonorg@gmail.com>
2025-02-19 02:37:32 -0300

    finish challenge

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -3,23 +3,13 @@ import { ClipboardText } from "@phosphor-icons/react"
 
 export function EmptyList() {
   return (
-    <div className={styles.todoList}>
-      <header>
-        <aside>
-          Tarefas criadas <span>0</span>
-        </aside>
-        <aside>
-          Concluidas <span>0</span>
-        </aside>
-      </header>
-      <div className={styles.emptyState}>
-        <div className={styles.iconContainer}>
-          <ClipboardText size={50} />
-        </div>
-        <div className={styles.textContainer}>
-          <p>Você ainda não tem tarefas cadastradas.</p>
-          <p>Crie tarefas e organize seus itens a fazer.</p>
-        </div>
+    <div className={styles.emptyState}>
+      <div className={styles.iconContainer}>
+        <ClipboardText size={50} />
+      </div>
+      <div className={styles.textContainer}>
+        <p>Você ainda não tem tarefas cadastradas.</p>
+        <p>Crie tarefas e organize seus itens a fazer.</p>
       </div>
     </div>
   );
